36afcc87074e1b995675f7719a20f82004daa84c
sebas101-1 <skdawg14@gmail.com>
2026-01-16 20:49:07 -0800

    Initial commit for HotPants generated swerve code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,145 +1,83 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import org.littletonrobotics.conduit.ConduitApi;
-import org.littletonrobotics.junction.LogFileUtil;
-import org.littletonrobotics.junction.LoggedRobot;
-import org.littletonrobotics.junction.Logger;
-import org.littletonrobotics.junction.LoggedPowerDistribution;
-import org.littletonrobotics.junction.networktables.NT4Publisher;
-import org.littletonrobotics.junction.wpilog.WPILOGReader;
-import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+import com.ctre.phoenix6.HootAutoReplay;
 
-import com.pathplanner.lib.commands.PathfindingCommand;
-
-import choreo.auto.AutoChooser;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
-public class Robot extends LoggedRobot {
-	public static Robot instance;
-	public static RobotContainer cont;
-	public static Command commandToRun;
-	public static boolean needToLookOtherWay;
 
-	public RobotContainer container;
+public class Robot extends TimedRobot {
+    private Command m_autonomousCommand;
 
+    private final RobotContainer m_robotContainer;
 
-	public Robot() {
-		super();
+    /* log and replay timestamp and joystick data */
+    private final HootAutoReplay m_timeAndJoystickReplay = new HootAutoReplay()
+        .withTimestampReplay()
+        .withJoystickReplay();
 
-		ConduitApi.getInstance().configurePowerDistribution(Constants.CAN.Misc.pdh, ModuleType.kRev.value);
+    public Robot() {
+        m_robotContainer = new RobotContainer();
+    }
 
-		switch(Constants.mode) {
-		case REAL -> {
-			Logger.addDataReceiver(new WPILOGWriter("/U/logs"));
-			Logger.addDataReceiver(new NT4Publisher());
-		}
+    @Override
+    public void robotPeriodic() {
+        m_timeAndJoystickReplay.update();
+        CommandScheduler.getInstance().run(); 
+    }
 
-		case SIM -> {
-			Logger.addDataReceiver(new NT4Publisher());
-		}
+    @Override
+    public void disabledInit() {}
 
-		case REPLAY -> {
-			this.setUseTiming(false); // Run as fast as possible
-			final String logPath = LogFileUtil.findReplayLog();
-			Logger.setReplaySource(new WPILOGReader(logPath));
-			Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
-		}
-		}
+    @Override
+    public void disabledPeriodic() {}
 
-		Logger.start();
+    @Override
+    public void disabledExit() {}
 
-		Robot.instance = this;
-		Robot.cont = new RobotContainer();
+    @Override
+    public void autonomousInit() {
+        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-		DriverStation.silenceJoystickConnectionWarning(true);
-	}
+        if (m_autonomousCommand != null) {
+            CommandScheduler.getInstance().schedule(m_autonomousCommand);
+        }
+    }
 
-	@Override
-	public void robotInit() {
-		// PathfindingCommand.warmupCommand().schedule();
-		cont.drivetrain.limelight.setIMUMode(1);
-	}
+    @Override
+    public void autonomousPeriodic() {}
 
-	@Override
-	public void robotPeriodic() {
-		CommandScheduler.getInstance().run();
-		LoggedPowerDistribution.getInstance(Constants.CAN.Misc.pdh, ModuleType.kRev);
-		cont.drivetrain.limelight.setRobotOrientation(cont.drivetrain.est.getEstimatedPosition().getRotation().getMeasure()); 
-	}
+    @Override
+    public void autonomousExit() {}
 
-	// DISABLED //
+    @Override
+    public void teleopInit() {
+        if (m_autonomousCommand != null) {
+            CommandScheduler.getInstance().cancel(m_autonomousCommand);
+        }
+    }
 
-	@Override
-	public void disabledInit() {
-		CommandScheduler.getInstance().cancelAll();
-		cont.drivetrain.limelight.setIMUMode(1);
-	}
+    @Override
+    public void teleopPeriodic() {}
 
-	@Override
-	public void disabledPeriodic() {
-		cont.drivetrain.disabledPeriodic();
-	}
+    @Override
+    public void teleopExit() {}
 
-	@Override
-	public void disabledExit() {
-	}
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll();
+    }
 
-	// AUTONOMOUS //
+    @Override
+    public void testPeriodic() {}
 
-	@Override
-	public void autonomousInit() {
-		// CommandScheduler.getInstance().cancelAll();
+    @Override
+    public void testExit() {}
 
-		// Get selected routine from the dashboard
-		// this.autonomousCommand = this.container.getAutonomousCommand();
-
-
-		// schedule the autonomous command (example)
-		// if(this.autonomousCommand != null) {
-		// 	this.autonomousCommand.schedule();
-		// }
-	}
-
-	@Override
-	public void autonomousPeriodic() {
-		cont.drivetrain.limelight.setIMUMode(2);
-	}
-
-	@Override
-	public void autonomousExit() {}
-
-	// TELEOP //
-
-	@Override
-	public void teleopInit() {
-		CommandScheduler.getInstance().cancelAll();
-		this.container.drivetrain.setDefaultCommand(this.container.drivetrain.joystickDrive);
-		// this.container.drivetrain.resetAngleWithLimelight();
-	}
-
-	@Override
-	public void teleopPeriodic() {
-	}
-
-	@Override
-	public void teleopExit() {}
-
-	// TEST //
-
-	@Override
-	public void testInit() {
-		CommandScheduler.getInstance().cancelAll();
-
-		this.container.drivetrain.setDefaultCommand(this.container.drivetrain.joystickDrive);
-	}
-
-	@Override
-	public void testPeriodic() {}
-
-	@Override
-	public void testExit() {}
+    @Override
+    public void simulationPeriodic() {}
 }
