107663821d1ba5896c55aac3be63f5e3e3ffed9f
Garrett Witowski <4096540+garrettwitowski@users.noreply.github.com>
2026-01-28 20:14:21 -0800

    Fixed some NT issues with collector + updated elastic layout

--- src/main/java/frc/robot/utils/ShooterDataCollectorIOReal.java
+++ src/main/java/frc/robot/utils/ShooterDataCollectorIOReal.java
@@ -2,35 +2,48 @@ package frc.robot.utils;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.BooleanSubscriber;
-import edu.wpi.first.networktables.DoubleSubscriber;
-import edu.wpi.first.networktables.StringSubscriber;
+import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.StringPublisher;
+import edu.wpi.first.networktables.DoubleEntry;
+import edu.wpi.first.networktables.BooleanEntry;
+import edu.wpi.first.networktables.StringEntry;
 
 public class ShooterDataCollectorIOReal implements ShooterDataCollectorIO {
     private final NetworkTable table;
     
-    private final DoubleSubscriber distanceInput;
-    private final DoubleSubscriber velocityInput;
-    private final DoubleSubscriber hoodAngleInput;
-    private final BooleanSubscriber successfulInput;
-    private final StringSubscriber notesInput;
-    private final BooleanSubscriber recordTrigger;
+    // Use entries so we can both read and write (publish default values)
+    private final DoubleEntry distanceInput;
+    private final DoubleEntry velocityInput;
+    private final DoubleEntry hoodAngleInput;
+    private final BooleanEntry successfulInput;
+    private final StringEntry notesInput;
+    private final BooleanEntry recordTrigger;
     private final BooleanPublisher recordingStatus;
 
     public ShooterDataCollectorIOReal() {
         NetworkTableInstance inst = NetworkTableInstance.getDefault();
         table = inst.getTable("ShooterData");
         
-        distanceInput = table.getDoubleTopic("distance_m").subscribe(0.0);
-        velocityInput = table.getDoubleTopic("velocity_rps").subscribe(0.0);
-        hoodAngleInput = table.getDoubleTopic("hood_angle_deg").subscribe(0.0);
-        successfulInput = table.getBooleanTopic("successful").subscribe(false);
-        notesInput = table.getStringTopic("notes").subscribe("");
-        recordTrigger = table.getBooleanTopic("record_trigger").subscribe(false);
+        // Create entries and publish default values so they appear in Elastic
+        distanceInput = table.getDoubleTopic("distance_m").getEntry(0.0);
+        velocityInput = table.getDoubleTopic("velocity_rps").getEntry(0.0);
+        hoodAngleInput = table.getDoubleTopic("hood_angle_deg").getEntry(0.0);
+        successfulInput = table.getBooleanTopic("successful").getEntry(false);
+        notesInput = table.getStringTopic("notes").getEntry("");
+        recordTrigger = table.getBooleanTopic("record_trigger").getEntry(false);
         recordingStatus = table.getBooleanTopic("recording_status").publish();
         
+        // Publish initial values
+        distanceInput.set(0.0);
+        velocityInput.set(0.0);
+        hoodAngleInput.set(0.0);
+        successfulInput.set(false);
+        notesInput.set("");
+        recordTrigger.set(false);
         recordingStatus.set(false);
+        
+        System.out.println("ShooterData NetworkTables topics published and ready for Elastic");
     }
 
     @Override
