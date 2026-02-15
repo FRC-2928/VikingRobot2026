f0107d5b496b3d9dd7739b2a4f2547d33030e3a7
alr11118 <alara.engin2008@gmail.com>
2026-02-15 12:28:27 -0800

    spotless

--- src/main/java/frc/robot/subsystems/IntakeIOReal.java
+++ src/main/java/frc/robot/subsystems/IntakeIOReal.java
@@ -76,7 +76,6 @@ public class IntakeIOReal implements IntakeIO {
                 .withForwardLimitAutosetPositionEnable(true)
                 .withForwardLimitAutosetPositionValue(Units.Degrees.of(0))
                 .withForwardLimitType(ForwardLimitTypeValue.NormallyOpen)
-                
                 .withReverseLimitRemoteCANdiS1(Constants.CAN.INTAKE_CANDI.getInstance())
                 .withReverseLimitEnable(true)
                 .withReverseLimitAutosetPositionEnable(true)
