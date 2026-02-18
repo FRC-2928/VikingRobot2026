ddd47cb197e63206bab2caa6be9bedd255948d8f
Tindleroot <tindleroot@gmail.com>
2026-02-18 15:31:34 -0800

    WARNING: code will not build. Pseudocode idea for integrating state machine in superstructure

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -5,15 +5,34 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.RobotContainer;
 import frc.robot.commands.Intake.ExtendAndRunIntake;
 
 public class Superstructure extends SubsystemBase {
+
+    public enum State {
+
+        IDLE(getDoNothingCommand()),
+        INTAKE(Commands.run(intake::extendAndIntake)),
+        AIM(superstructure.aimWithTransition()),
+        SHOOT(superstructure.shoot());
+
+        private final Trigger isCurrentState;
+        private final Command command;
+
+        State(Command doWhileTrue) {
+            isCurrentState = new Trigger(() -> superstructure.isCurrentState(this));
+            isCurrentState.whileTrue(doWhileTrue);
+        }
+    }
+
     private RobotContainer cont;
+    private State currentState;
 
     public Superstructure(RobotContainer cont) {
         this.cont = cont;
+        this.currentState = getStartingState();
     }
 
     // Runs flywheels and kicker. Command will not end on its own
