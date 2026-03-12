b70bb8dd6a7e657375920fe722b8b3cc575c5a49
alr11118 <alara.engin2008@gmail.com>
2026-03-11 20:36:20 -0700

    added velocity control to hopper and indexer, PID's still need to be tuned.

--- src/main/java/frc/robot/subsystems/IndexerIOReal.java
+++ src/main/java/frc/robot/subsystems/IndexerIOReal.java
@@ -5,8 +5,10 @@ import java.util.List;
 import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
@@ -28,27 +30,43 @@ public class IndexerIOReal implements IndexerIO {
     // Collection of all status signals
     private List<BaseStatusSignal> mStatusSignals;
 
+    final VelocityVoltage indexerVelocityVoltage;
+
     public IndexerIOReal() {
         this.indexer = new TalonFX(Constants.CAN.CTRE.indexer, Constants.CAN.CTRE.bus);
         this.indexerVelocity = this.indexer.getRotorVelocity();
 
         BaseStatusSignal.setUpdateFrequencyForAll(100, indexerVelocity);
-        TalonFXConfiguration config = new TalonFXConfiguration();
+        TalonFXConfiguration indexerConfig = new TalonFXConfiguration();
         CurrentLimitsConfigs currentLimitsConfigs = new CurrentLimitsConfigs();
-        config.CurrentLimits = currentLimitsConfigs;
+        indexerConfig.CurrentLimits = currentLimitsConfigs;
 
         currentLimitsConfigs.StatorCurrentLimit = 40; // the peak current, in amps
-        indexer.getConfigurator().apply(config); // apply the config settings; this selects the quadrature encode
+        indexer.getConfigurator().apply(indexerConfig); // apply the config settings; this selects the quadrature encode
 
-        config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-        config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-        config.CurrentLimits.SupplyCurrentLimitEnable = true;
-        config.CurrentLimits.StatorCurrentLimitEnable = true;
+        indexerConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        indexerConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        indexerConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+        indexerConfig.CurrentLimits.StatorCurrentLimitEnable = true;
 
-        config.CurrentLimits.SupplyCurrentLimit = 60.0;
-        config.CurrentLimits.StatorCurrentLimit = 120.0;
+        indexerConfig.CurrentLimits.SupplyCurrentLimit = 60.0;
+        indexerConfig.CurrentLimits.StatorCurrentLimit = 120.0;
 
-        indexer.getConfigurator().apply(config);
+        //TODO: actually tune these PID's
+        Slot0Configs indexerFloorSlot0Configs = new Slot0Configs();
+        indexerFloorSlot0Configs.kS = 0.1; // Add 0.1 V output to overcome static friction
+        indexerFloorSlot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+        indexerFloorSlot0Configs.kP = 0.11; // An error of 1 rps results in 0.11 V output
+        indexerFloorSlot0Configs.kI = 0; // no output for integrated error
+        indexerFloorSlot0Configs.kD = 0; // no output for error derivative
+
+        // PID Values
+        indexerConfig.Slot0 = indexerFloorSlot0Configs;
+
+        indexer.getConfigurator().apply(indexerConfig); // apply the config settings; this selects the quadrature encode
+
+        // create a velocity closed-loop request, voltage output, slot 0 configs
+        this.indexerVelocityVoltage = new VelocityVoltage(0).withSlot(0);
 
         this.indexerAngularVelocitySignal = this.indexer.getVelocity();
         this.indexerStatorCurrentSignal = this.indexer.getStatorCurrent();
