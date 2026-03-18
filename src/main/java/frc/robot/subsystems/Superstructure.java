9b353bcc4694be6fad961a280decf16822ac49ce
sebas101-1 <skdawg14@gmail.com>
2026-03-17 17:17:46 -0700

    changed aimHome to use home lookup table

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -268,11 +268,6 @@ public class Superstructure extends SubsystemBase {
                 // Add OVERRIDE_SHOOTING to the set of active overrides
                 mActiveOverrides.add(StateOverrides.OVERRIDE_SHOOTING);  // Internally: 01 (bit 0 is now 1)
                 currentState = RobotState.SHOOTING; // transition directly into shooting mode
-                // if (mRobotContainer.drivetrain.isAtHome()) {
-                //     currentState = RobotState.SHOOTING; // transition directly into shooting mode
-                // } else {
-                //     currentState = RobotState.SHOOT_HOME; // transition directly into shooting towards home
-                // }
                 Logger.recordOutput("Superstructure/OverrideState", "SHOOTING");
                 break;
             }
@@ -283,6 +278,7 @@ public class Superstructure extends SubsystemBase {
                 /*if () {
                     
                 }*/
+                break;
             }
             // ... other overrides here
             case OVERRIDE_NONE:
